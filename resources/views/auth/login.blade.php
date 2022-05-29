07c0337a906663c317812a6999a7b405335ca51b
osba0 <osm.cre.dev@gmail.com>
2022-05-28 18:55:34 -0700

    Correction button connexion

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -65,7 +65,9 @@
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Connexion') }}
+                                   {{ __('Connexion') }} <span class="material-symbols-outlined align-middle">
+login
+</span>
                                 </button>
 
                                
